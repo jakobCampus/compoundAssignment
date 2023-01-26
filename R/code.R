dac59c1b5fc45d9fe0e3742e598151a3d441303c
jakobCampus <jakob.becker@campus.lmu.de>
2023-01-26 19:52:10 +0100

    added manual

--- R/code.R
+++ R/code.R
@@ -1,1 +1,17 @@
+#' @title compoundAssignment [`+=`]
+#' 
+#' @description Enables the user the option to use the [`+=`] compound assignment.
+#' 
+#' @param t (`atomic(1)`)\cr
+#'   The original variable value.
+#' @param s (`atomic(1)`)\cr
+#'   The value to be added.
+#'   
+#' @examples
+#' value <- 5
+#' print(value)
+#' value += 7
+#' print(value)
+#' 
+#' @export
 `%+=%` = function(t, s) eval.parent(substitute(t <- t + s))
